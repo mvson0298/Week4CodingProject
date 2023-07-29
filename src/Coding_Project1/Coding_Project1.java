5f805f7027a34ecf1335ba955fea216ea9a4c3d4
mvson0298 <140126685+mvson0298@users.noreply.github.com>
2023-07-29 14:01:30 -0600

    Adding comments to questions

--- src/Coding_Project1/Coding_Project1.java
+++ src/Coding_Project1/Coding_Project1.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 public class Coding_Project1 {
 
 	public static void main(String[] args) {
-//1)		
+//1)	taking the last element of an array and subtracting the first element
 		int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
 		
 		int additionSum = 0;
@@ -15,7 +15,7 @@ public class Coding_Project1 {
 
 		System.out.println(additionSum);
 
-//b)		
+//b)	showing that the code is dynamic and works for any array lengths
 		int[] ages2 = {3, 9, 23, 64, 2, 8, 28, 93, 67};
 		
 		int additionSum2 = 0;
@@ -24,7 +24,7 @@ public class Coding_Project1 {
 		
 		System.out.println(additionSum2);
 		
-//c)
+//c)	finding the average w/ an enhanced for loop
 		double sum = 0.0;
 		
 		for (double age : ages) {
@@ -34,7 +34,7 @@ public class Coding_Project1 {
 		double average = sum / ages.length;
 		System.out.println("The average of this array is " + average);
 		
-//2)
+//2)	creating a string array then locating the average # of characters with .length(). Then concatenating all of the names separated by a space
 		String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
 	
 		sum = 0;
@@ -55,7 +55,7 @@ public class Coding_Project1 {
 		
 //4)	using arrayName[0] will always access the first element of an array as arrays are 0 based. (i.e. ages[0] = 3)
 		
-//5)
+//5)	creating new int array and a for loop to count out the characters that are annotated in the array
 		int[] nameLengths = new int[names.length];
 		for (int i = 0; i < names.length; i++) {
 			nameLengths[i] = names[i].length(); 
@@ -67,7 +67,8 @@ public class Coding_Project1 {
 			sum += nameLengths[i];
 		}
 		System.out.println(sum);
-//7)
+//7)	method that uses an string/int, empty string and a for loop to add the inserted word based on the integer noted in the method
+		
 		System.out.println(duplicateWord("Hello", 6));
 //		private static String duplicateWord(String word, int n) {
 //			String duplicatedWord = "";
@@ -76,14 +77,14 @@ public class Coding_Project1 {
 //			}
 //			return duplicatedWord;
 	
-//8)
+//8)	method concatenating a first and last name seperated by a space
 		System.out.println(getFullName("Mason", "Curtis"));
 		
 //		private static String getFullName(String firstN, String lastN) {
 //			return firstN + " " + lastN;
 //		}
 	
-//9)
+//9)	using a boolean method to return if the elements added together are greater than 100.
 		System.out.println(checkIfSumGreaterThan100(ages));
 		
 //		private static boolean checkIfSumGreaterThan100(int[] arr) {
@@ -95,7 +96,7 @@ public class Coding_Project1 {
 //			return sum > 100;
 		
 		
-//10)	
+//10)	finding average by adding the elements of doubleArray together then dividing that sum by the length of the array as done previously.
 		double[] doubleArray = {0.32, 4.65, 6.45, 3.82};
 		System.out.println("The average of doubleArray " + getAverageOfDoubleArray(doubleArray));
 //		private static double getAverageOfDoubleArray(double[] doubleArray) {
@@ -116,14 +117,14 @@ public class Coding_Project1 {
 //		}
 		
 		
-//12)
+//12)	just uses a boolean method to take into a account a t/f statement and a value.
 		System.out.println("If true, I get a drink, if false I don't: " + willBuyDrink(false, 10.98));
 //		
 //		private static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
 //			return isHotOutside && moneyInPocket > 10.50;
 //		}
 		
-//13)	
+//13)	Method that identifies the current date. needed to import LocalDate to use as a data type in order to return the current data. its important to always know what day it is.
 		System.out.println("What is today's Date?");
 		System.out.println(todaysDate());
 		
@@ -132,7 +133,7 @@ public class Coding_Project1 {
 //			}
 	}
 //	private static LocalDate todaysDate() {
-//		
+//		this method calculates the current date because it's always important to know what day it is.
 //		return java.time.LocalDate.now();
 //		}
 
